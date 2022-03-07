ef7e6faef11ade532ecd18b78cbb45826fa35a3b
minukheo1022@gmail.com <minukheo1022@gmail.com>
2022-03-07 16:37:15 +0900

    update

---
+++ 복습 - 구현 ,스택 ,큐/달팽이1913.py
@@ -0,0 +1,20 @@
+"""
+
+
+"""
+
+import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+M = int(input().rstrip())
+
+#nxn 배열 생성
+arr = [[] for _ in range(N)]
+
+
+
+print(arr)
+
+
+print("123")
---
+++ 복습 - 구현 ,스택 ,큐/정리노트.py
@@ -0,0 +1,2 @@
+#1 : nxn 배열
+arr = [[] for _ in range(5)]
